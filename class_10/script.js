2af4a9e5888f42607b8ae0abdc8733784fdd4374
Malihanaz09 <malihanaz541@gmail.com>
2025-10-13 21:33:05 +0500

    add_class_10

---
+++ class_10/script.js
@@ -0,0 +1,96 @@
+console.log("Hello world");
+
+// alert("Hello from external JS file");
+
+// prompt("What is your name?");
+
+full_name = "Maliha";
+$fullName09 = "Ali"
+console.log(full_name);
+console.log($fullName09);
+age = 25;
+age = 26
+console.log(age);
+
+a = 10;
+A = 20;
+console.log(a);
+console.log(A);
+
+Console = "Test";
+console.log(Console);
+
+let city = "karachi";
+city = "lahore";
+console.log(city);
+
+const country = "Pakistan";
+// country = "India";
+console.log(country);
+
+let b = 2.5;
+console.log(a+b);
+
+//boolean
+let isFollow = false;
+console.log(isFollow);
+
+//null
+let student = null;
+console.log(student);
+
+//undefined
+let teacher;
+console.log(teacher);
+
+//array
+let fruits = ["apple", "banana", "mango"];
+console.log(fruits);
+
+//object
+let student1 = {
+    name: "Ali",
+    age: 20,
+    rollNo: 101,
+}
+console.log(student1);
+
+//arthmetic operators
+let x = 8;
+let y = 3;
+console.log( x + y);
+console.log( x - y);
+console.log(x*y);
+console.log(x/y);
+
+//increment
+let num1 = 5;
+console.log(num1);
+num1++;
+console.log(num1);
+num1++;
+console.log(num1);
+
+//decrement
+let num2 = 20;
+console.log(num2);
+num2--;
+console.log(num2);
+num2--;
+console.log(num2);
+
+//modulus
+let num3 = 10;
+let num4 = 3;
+console.log(num3 % num4);
+
+//exponentiation
+let num5 = 2;
+let num6 = 3;
+console.log(num5 ** num6);
+// 2*2*2=8
+
+let num7 = 3;
+let num8 = 4;
+console.log(num7 ** num8);
+// 3*3*3*3=81
